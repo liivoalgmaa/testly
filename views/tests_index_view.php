d9b58eec6b16bcab6e5dcdf63e68c53eb04c9207
Liivo Algmaa <liivo.algmaa@khk.ee>
2013-04-19 13:08:50 +0300

    Add delete function to remove tests.

--- views/tests_index_view.php
+++ views/tests_index_view.php
@@ -1,7 +1,6 @@
-<p>
-	<a class="btn btn-large btn-info" href="http://google.com">Lisa uus test</a>
+<p> <a class="btn btn-large btn-primary" href="https://www.google.ee">Lis uus test </a>
 </p>
-<table id="tests-table" class= "table table-bordered table-striped">
+<table id="tests-table" class="table table-bordered table striped">
 	<thead>
 	<th>Testi nimi</th>
 	<th>Koostaja</th>
@@ -9,13 +8,19 @@
 	<th>Tegevused</th>
 	</thead>
 	<tbody>
-	<? if(! empty($tests)): foreach($tests as $test): ?>
-	<tr id="test<?=$test['test_id'] ?>">
-		<td><?=$test['name']?></td>
-		<td><?=$test['username']?></td>
-		<td><?=$test['date']?></td>
-		<td><?="vaata" ?><i class= "icon-pencil"></i></td>
-	</tr>
-	<? endforeach; endif ?>
+	<?if (!empty($tests)):foreach($tests as $test):?>
+		<tr id="test"<?=$test['test_id']?>>
+			<td><?=$test['name']?></td>
+			<td><?=$test['username']?></td>
+			<td><?=substr($test['date'],0,10)?></td>
+			<td>
+				<i class="icon-pencil"></i>
+
+				<a href="<?=BASE_URL?>tests/remove/<?=$test['test_id']?>">
+					<i class="icon-trash"></i>Kustuta</a> </td>
+
+		</tr>
+	<?endforeach;endif?>
 	</tbody>
+
 </table>
